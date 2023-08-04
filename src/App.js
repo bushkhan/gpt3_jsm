fd0c8a360ad11cec7d36270617389795835ae699
Bushra Khan <bushrakhan2212002@gmail.com>
2023-08-04 11:19:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <h1>GPT-3</h1>
+    </div>
   )
-}
+    }
 
 export default App
